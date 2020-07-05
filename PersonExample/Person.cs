4c7875c9be3dddccdde3107c8b3bb5fe9d281278
Tomer <Tomer.halfon773@gmail.com>
2020-07-05 19:28:02 +0300

    Orgenized the solution and add the first HW

---
+++ PersonExample/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonExample
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Person(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Age: {Age}";
+        }
+    }
+}
---
+++ PersonExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Person(27, "Tomer"));
+        }
+    }
+}
