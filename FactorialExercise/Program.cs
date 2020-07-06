45eddee19713726a5f842578e6e240d6efdd5565
Tomer <Tomer.halfon773@gmail.com>
2020-07-06 13:24:39 +0300

    added a few HW Assigments

---
+++ FactorialExercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialExercise
+{
+    class Program
+    {
+        //calculate factorial
+        static long Factorial(long num)
+        {
+            long sum = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                sum *= i;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            string inputString = "";
+            //run as long as the input is stop
+            while (!inputString.Equals("stop"))
+            {
+                //request a number
+                Console.Write("please enter a number to calculate it's factorial\n(you can type in 'stop' to stop the program): ");
+                inputString = Console.ReadLine();
+
+                //if it's a valid number
+                if (long.TryParse(inputString, out long inputNum))
+                {
+                    long res = Factorial(inputNum);
+                    Console.WriteLine($"The Factorial of { inputString } is { res }");
+                }
+            }
+        }
+    }
+}
---
+++ RockPaperScissorsGame/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsGame
+{
+    class Program
+    {
+        static void PrintOptions()
+        {
+            //selections
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Rock");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Paper");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Scissors");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static string GetComputerSelection(string playerSelection) 
+        {
+            string res;
+            switch (playerSelection)
+            {
+                case "Rock":
+                    res =  "Paper";
+                    break;
+
+                case "Paper":
+                    res = "Scissors";
+                    break;
+
+                case "Scissors":
+                    res = "Rock";
+                    break;
+
+                default:
+                    res = "";
+                    break;
+            }
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            string inputString;
+            string computerSelection;
+
+            //instructions
+            Console.WriteLine("Welcome to 'Rock Paper Scissors'");
+            Console.WriteLine("you start the game by selecting one of the options\nthe game can last forever so type 'Stop' to stop the program");
+
+            //game loop
+            do
+            {
+                //show the options the player have and request a selection
+                Console.WriteLine("\nEnter your choice");
+                PrintOptions();
+                Console.WriteLine();
+                inputString = Console.ReadLine();
+
+                //call the func and get the computer selection
+                computerSelection = GetComputerSelection(inputString);
+
+                //if not the default case (a valid input)
+                if (!computerSelection.Equals(""))
+                {
+                    Console.WriteLine($"\nI chose the better weapon in this children game\n{computerSelection} beats {inputString}");
+                }
+                else if (!inputString.Equals("Stop"))
+                {
+                    Console.WriteLine($"\n{inputString} is not a valid selection");
+                }
+
+            } while (!inputString.Equals("Stop"));
+
+            //display a message to indicate closing of the game
+            Console.WriteLine("Closing the game");
+        }
+    }
+}
