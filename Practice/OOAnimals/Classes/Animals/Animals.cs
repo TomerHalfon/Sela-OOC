8fe423a7ccfeb49e3e66ced1b858d29a64db9228
Tomer <Tomer.halfon773@gmail.com>
2020-07-07 13:15:43 +0300

    Added the practice lessons dir, and created the first project

---
+++ Practice/OOAnimals/Classes/Animals/Animals.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAnimals.Classes.Animals
+{
+    public abstract class Animals
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; } = 0;
+        public int Speed { get; private set; } = 1;
+        public double Weight { get; private set; }
+        public Gender Gender { get; private set; }
+        public Position MyPosition { get; private set; }
+
+        public void Move(int deltaX, int deltaY)
+        {
+            MyPosition.Move(deltaX, deltaY);
+        }
+    }
+}
---
+++ Practice/OOAnimals/Classes/Structs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAnimals.Classes
+{
+    public struct Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void Move(int deltaX, int deltaY)
+        {
+            X += deltaX;
+            Y += deltaY;
+        }
+    }
+}
