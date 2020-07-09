9f34162bb185854810137102a320621172bf4df6
Tomer <Tomer.halfon773@gmail.com>
2020-07-09 14:24:09 +0300

    added title case to name var

--- LessonsFiles/PersonClassExc/Program.cs
+++ LessonsFiles/PersonClassExc/Program.cs
@@ -8,25 +8,36 @@ namespace PersonClassExc
     {
         static void Main(string[] args)
         {
-            //person use
-            Person perosn = new Person("Tomer", 27);
-            Console.WriteLine(perosn);
-
+            ////person use
+            //Person perosn = new Person("Tomer", 27);
+            //Console.WriteLine(perosn);
 
             //format exercise
-            //request a name
+            //create a us culture
+            CultureInfo usCulture = new CultureInfo("en-US");
+
+            //set current culture to us
+            Thread.CurrentThread.CurrentCulture = usCulture;
+
+            //request name
             Console.Write("Please enter emplyee name: ");
-            string name = Console.ReadLine();
+            string name = usCulture.TextInfo.ToTitleCase(Console.ReadLine());
+            while (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("invalid input!, try again");
+                name = usCulture.TextInfo.ToTitleCase(Console.ReadLine());
+            }
 
             //request salary
-            Console.Write("Please enter {0} salary: ",name);
-            double salary = double.Parse(Console.ReadLine());
-
-            //set culture to us
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            double salary; 
+            Console.Write("Please enter {0}'s salary: ", name);
+            while (!double.TryParse(Console.ReadLine(), out salary))
+            {
+                Console.WriteLine("invalid input!, try again");
+            }
 
             //format and print
-            Console.WriteLine("Employee nane {0}, Salary {1:C}",name,salary);
+            Console.WriteLine("Employee name {0}, Salary {1:C}", name, salary);
         }
     }
 }
