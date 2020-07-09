59a6c87a862242624f56d9567f97c3a4856bfeb6
Tomer <Tomer.halfon773@gmail.com>
2020-07-09 14:53:23 +0300

    formating project

--- LessonsFiles/PersonClassExc/Program.cs
+++ LessonsFiles/PersonClassExc/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Globalization;
-using System.Threading;
 
 namespace PersonClassExc
 {
@@ -8,36 +6,9 @@ namespace PersonClassExc
     {
         static void Main(string[] args)
         {
-            ////person use
-            //Person perosn = new Person("Tomer", 27);
-            //Console.WriteLine(perosn);
-
-            //format exercise
-            //create a us culture
-            CultureInfo usCulture = new CultureInfo("en-US");
-
-            //set current culture to us
-            Thread.CurrentThread.CurrentCulture = usCulture;
-
-            //request name
-            Console.Write("Please enter emplyee name: ");
-            string name = usCulture.TextInfo.ToTitleCase(Console.ReadLine());
-            while (string.IsNullOrEmpty(name))
-            {
-                Console.WriteLine("invalid input!, try again");
-                name = usCulture.TextInfo.ToTitleCase(Console.ReadLine());
-            }
-
-            //request salary
-            double salary; 
-            Console.Write("Please enter {0}'s salary: ", name);
-            while (!double.TryParse(Console.ReadLine(), out salary))
-            {
-                Console.WriteLine("invalid input!, try again");
-            }
-
-            //format and print
-            Console.WriteLine("Employee name {0}, Salary {1:C}", name, salary);
+            //person use
+            Person perosn = new Person("Tomer", 27);
+            Console.WriteLine(perosn);
         }
     }
 }
---
+++ LessonsFiles/StringFormating/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+namespace StringFormating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //culture and formating
+            //create a us culture
+            CultureInfo usCulture = new CultureInfo("en-US");
+
+            //set current culture to us
+            Thread.CurrentThread.CurrentCulture = usCulture;
+
+            //request name
+            Console.Write("Please enter emplyee name: ");
+            string name = usCulture.TextInfo.ToTitleCase(Console.ReadLine());
+            while (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("invalid input!, try again");
+                name = usCulture.TextInfo.ToTitleCase(Console.ReadLine());
+            }
+
+            //request salary
+            double salary;
+            Console.Write("Please enter {0}'s salary: ", name);
+            while (!double.TryParse(Console.ReadLine(), out salary))
+            {
+                Console.WriteLine("invalid input!, try again");
+            }
+
+            //format and print
+            Console.WriteLine("Employee name {0}, Salary {1:C}", name, salary);
+        }
+    }
+}
