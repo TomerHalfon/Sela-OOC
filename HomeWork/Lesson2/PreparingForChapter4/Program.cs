17e1cb085ff7bc4ea57c20ce1911fc5c87cdbd28
Tomer <Tomer.halfon773@gmail.com>
2020-07-10 10:16:22 +0300

    added the latest HW assigment

---
+++ HomeWork/Lesson2/PreparingForChapter4/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace PreparingForChapter4
+{
+    class Program
+    {
+        #region for ease of use only!
+        const int AppRecWidth = 20;
+        const int AppRecHeight = 10;
+        const int AppRecX = 1;
+        const int AppRecY = 0;
+        private static int RequestNumber(string msg)
+        {
+            int num;
+            Console.Write(msg);
+            while (!int.TryParse(Console.ReadLine(), out num))
+            {
+                Console.WriteLine("Invalid input!\nTry again...");
+            }
+            return num;
+        } 
+        #endregion
+        static void Main()
+        {
+            //Create a user defiend Rectangle
+            Rectangle userRectangle = new Rectangle(
+                RequestNumber("Please enter the X position of the rectangle: "),
+                RequestNumber("Please enter the Y position of the rectangle: "),
+                RequestNumber("Please enter the width of the rectangle: "),
+                RequestNumber("Please enter the height of the rectangle: ")
+                );
+
+            //print the user rectangle
+            Console.WriteLine("\nUser Rectanagle\n" + userRectangle);
+
+            //Create appliacation Rectangle using hard coded data
+            Rectangle applicationRectangle = new Rectangle(AppRecX, AppRecY, AppRecWidth, AppRecHeight);
+
+            //print the application rectangle
+            Console.WriteLine("Application Rectanagle\n" + applicationRectangle);
+
+            //compare the two using rectangle IsSizeEqual
+            Console.WriteLine($"Comparing two Rectangles by size...");
+            if (applicationRectangle.IsSizeEqual(userRectangle))
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("not Equal");
+            }
+
+            //print the smaller Rectangle
+            Console.WriteLine($"The minimal rectangle is {applicationRectangle.Minimum(userRectangle)}");
+
+            //Set app rectangle's data to user rectangle's data
+            Console.WriteLine("updating application rectangle to user rectangle data...");
+            applicationRectangle.Assign(userRectangle);
+
+            //Compare the two using overriden Equals method(compares data)
+            Console.WriteLine($"Comparing two Rectangles using Equals method...");
+            if (applicationRectangle.Equals(userRectangle))
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("not Equal");
+            }
+
+            //comparing with == (in refrence types compares adress in heap)
+            Console.WriteLine("Comparing by adress (==) operator");
+            if (applicationRectangle == userRectangle)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("not Equal");
+            }
+
+            //Assign user's rectangle to application rectangle using the (=) operator (assigns adress in heap)
+            Console.WriteLine("Assigning user's rectangle to application rectangle using the (=) operator");
+            applicationRectangle = userRectangle;
+
+            //conpare the two by adress again (==) now after assigning the adress 
+            Console.WriteLine("Comparing by adress (==) operator after the change");
+            if (applicationRectangle == userRectangle)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("not Equal");
+            }
+        }
+    }
+}
---
+++ HomeWork/Lesson2/PreparingForChapter4/Rectangle.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+
+namespace PreparingForChapter4
+{
+    class Rectangle
+    {
+        //position
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        //size
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        //constructor
+        public Rectangle(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+        //returns the area of the rectangle
+        public int Area()
+        {
+            return Width * Height;
+        }
+        //change the size to a new size
+        public void Resize(int newWidth,int newHeight)
+        {
+            Width = newWidth;
+            Height = newHeight;
+        }
+        //change position to a new one
+        public void Move(int newX,int newY)
+        {
+            X = newX;
+            Y = newY;
+        }
+        //clone a rectangle data on to this 
+        public void Assign(Rectangle otherRec)
+        {
+            X = otherRec.X;
+            Y = otherRec.Y;
+            Width = otherRec.Width;
+            Height = otherRec.Height;
+        }
+        //returns true if the area is the same
+        public bool IsSizeEqual(Rectangle otherRec)
+        {
+            return Area() == otherRec.Area();
+        }
+        //returns the smaller (by area) rectangle
+        public Rectangle Minimum(Rectangle otherRec)
+        {
+            return (Area() < otherRec.Area()) ? this : otherRec;
+        }
+        //print the rectangle as a string
+        public override string ToString()
+        {
+            return $"Positioned at ({X},{Y})\n\n\n{DrawRectangle()}";
+        }
+        //draw the rectangle for ease of use
+        private string DrawRectangle()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    sb.Append("*   ");
+                }
+                sb.Append("\n");
+            }
+            return sb.ToString();
+        }
+        //compare the two by width and height (ignoring position)
+        public override bool Equals(object obj)
+        {
+            Rectangle otherRec = (Rectangle)obj;
+            return Width == otherRec.Width && Height == otherRec.Height;
+        }
+        //returns a unique hash code based on the string representation of this rectangle
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+    }
+}
