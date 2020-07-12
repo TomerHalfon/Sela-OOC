67948c9bb33906aceea544da7e467624b8018f11
Tomer <Tomer.halfon773@gmail.com>
2020-07-12 18:36:05 +0300

    finished the added assigment for Person Exc

--- LessonsFiles/PersonClassExc/Person.cs
+++ LessonsFiles/PersonClassExc/Person.cs
@@ -3,27 +3,50 @@ namespace PersonClassExc
 {
     class Person
     {
-        public int Age { get; private set; }
+        // personCount is a static property that holds a counter that we add to when we call the full constructor
+        //(there's no need to initialize it in a separate static constructor, since there is no logic needed)
+        public static int personCount { get; private set; } = 0;
+        //Id proerty canot be changed at all, not even from within the class.
+        //it gets the data only when we initialize a new instance of this class
+        public int Id { get; } = personCount;
         public string Name { get; private set; }
-
+        public int Age { get; private set; }
         public Person(string name, int age)
         {
-            Age = age;
             Name = name;
+            Age = age;
+
+            //increase the counter 
+            ++personCount;
         }
 
+        //Gets only the name parameter and passes a hard coded age to the full constructor
+        public Person(string name) : this(name, 30) { }
         public override string ToString()
         {
-            return $"Name: {Name}, Age: {Age}";
+            return $"ID: {Id}, Name: {Name}, Age: {Age}";
         }
         public override bool Equals(object obj)
         {
+            //create a res variable and assign it as false
             bool res = false;
-            Person toCompare = (Person)obj;
-            if (Name == toCompare.Name&&Age == toCompare.Age)
+
+            //if the type of obj(we get it with the GetType() method) is the same type as Class Person
+            //(we do this using the typeof(insert name of Object here) operator)
+            if (obj.GetType() == typeof(Person))
             {
-                res = true;
+                //now that we're sure that obj is of type Person, we can safely DownCast(using (Person))
+                Person toCompare = (Person)obj;
+
+                //comparing the properties of the two objects 
+                if (Name == toCompare.Name && Age == toCompare.Age)
+                {
+                    res = true;
+                }
             }
+
+            //if the types don't match(therefore they can't be equal and so we can skip some processing)
+            //return false if res wasn't changed
             return res;
         }
     }
--- LessonsFiles/PersonClassExc/Program.cs
+++ LessonsFiles/PersonClassExc/Program.cs
@@ -6,13 +6,29 @@ namespace PersonClassExc
     {
         static void Main()
         {
-            //person use
-            Person perosn = new Person("Tomer", 27);
-            Console.WriteLine(perosn);
+            //create person with full constructor
+            Person person1 = new Person("Person1", 27);
+            Person person2 = new Person("Person2", 26);
+
+            //create person with only one parameter (age defaults to 30)
+            Person person3 = new Person("Person3");
+
+            //Print the People
+            Console.WriteLine(person1);
+            Console.WriteLine(person2);
+            Console.WriteLine(person3);
+            Console.WriteLine();
 
             //equal override
-            Person person2 = new Person("Tomer", 26);
-            Console.WriteLine(perosn.Equals(person2));
+            Console.WriteLine($"Are person1 and person2 equal?\n(do they share the same name and age): {person1.Equals(person2)}\n");
+
+            //check if person is of type Person using GetType()
+            Console.WriteLine($"is person of type Person? {person1.GetType() == typeof(Person)}\n");
+
+            //print how many instances of Class Person were created using the static property
+            //(notice we can only get the value, we can't change it from outside of the class)
+            Console.WriteLine($"We created {Person.personCount} diffrent instances of class Person");
+
         }
     }
 }
