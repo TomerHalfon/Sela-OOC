b60aa57ad4e1a2de1c6a5f41d4522f0ae67879a4
Tomer <Tomer.halfon773@gmail.com>
2020-07-12 12:16:04 +0300

    Fixed wording

--- LessonsFiles/TypeClass/Program.cs
+++ LessonsFiles/TypeClass/Program.cs
@@ -20,7 +20,7 @@ namespace TypeClass
             
             if (t1 == t2)
             {
-                Console.WriteLine("They have the same adress in the heap");
+                Console.WriteLine("They have the same adress in the heap\n");
             }
 
             //Type class contains all of the information about the Person Class. for example:
@@ -29,7 +29,7 @@ namespace TypeClass
             //the dll information
             Console.WriteLine($"The dll in which Class Person is at is: {t1.Assembly}");
             //is it public
-            Console.WriteLine($"Is the type public?: {t1.IsPublic}");
+            Console.WriteLine($"Is the type public?: {t1.IsPublic}\n");
             //and many more...
 
             //where is it usefull?
@@ -37,15 +37,16 @@ namespace TypeClass
             //it is VERY dangerous to (cast) any object, 
             //because it's possible to use objects which are the derived class and store them in the base format.
             //for example
-            object obj1 = new Person("Person", 26);
+            object obj1 = new Book("Book", 150);
 
-            //this code is bad and will cause a compiler erorr
-            //Person p3 = (Book)obj;
+            //this code is bad and will cause an Exaption
+            //Person p3 = (Person)obj1;
 
             //Therefore it's a best practice to verify it's type before you DownCast(cast)
             if (obj1.GetType() == typeof(Person))
             {
-                Console.WriteLine("obj is of type Person");
+                //only now we can DownCast
+                Person p3 = (Person)obj1;
             }
 
             //create a diffrent class obj
@@ -53,7 +54,7 @@ namespace TypeClass
 
             //use the Equals method
             Console.WriteLine($"is book Equals to P1? {p1.Equals(book)}");
-            Console.WriteLine($"is p1 Equals to p2? {p1.Equals(p2)}");
+            Console.WriteLine($"is p1 Equals to p2? {p1.Equals(p2)}\n");
         }
     }
 }
