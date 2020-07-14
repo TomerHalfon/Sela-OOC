b0707bcdff375f22a14511c0d81343732d8a37b8
Tomer <Tomer.halfon773@gmail.com>
2020-07-14 16:05:08 +0300

    added billing proj

---
+++ LessonsFiles/BillingSystem/Classes/Customer.cs
@@ -0,0 +1,32 @@
+﻿namespace BillingSystem.Classes
+{
+    class Customer
+    {
+        // static counter
+        public static int CustomerCount { get; private set; } = 0;
+
+        //data members
+        double _balance;
+
+        //read only Id, can only be initialized
+        public int Id { get; }
+        public string Name { get; private set; }
+        public double Balance
+        {
+            get { return _balance; }
+            private set { _balance = value; }
+        }
+
+        //full constructor with default parameter balance
+        public Customer(string name, double balance = 0)
+        {
+            Name = name;
+            Balance = balance;
+            Id = ++CustomerCount;
+        }
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Balance: {Balance}";
+        }
+    }
+}
---
+++ LessonsFiles/BillingSystem/Program.cs
@@ -0,0 +1,18 @@
+﻿using BillingSystem.Classes;
+using System;
+
+namespace BillingSystem
+{
+    class Program
+    {
+        static void Main()
+        {
+            Customer c1 = new Customer("P1");
+            Customer c2 = new Customer("P2", 100d);
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(Customer.CustomerCount);
+        }
+    }
+}
