ec7dff78ad93c88a6b9a618305b59366afa9ca3c
Tomer <Tomer.halfon773@gmail.com>
2020-07-28 18:30:11 +0300

    Added The Home Assignment Sets

---
+++ Assignments/Sets/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sets
+{
+    class Program
+    {
+        //a rng for the program
+        static Random rng = new Random();
+
+        #region Functions for ease of use
+        static void ReRollData(params Set[] sets)
+        {
+            foreach (Set item in sets)
+            {
+                for (int i = 0; i < 12; i++)
+                {
+                    item.Insert(rng.Next(1001));
+                }
+            }
+        }
+        static int RequestNumber(string msg)
+        {
+            int num;
+            Console.Write(msg);
+            while (!int.TryParse(Console.ReadLine(), out num))
+            {
+                Console.WriteLine("Invalid input!\nTry again...");
+            }
+            return num;
+        } 
+        #endregion
+        static void Main()
+        {
+            //create two sets (s1,s2)
+            Set s1 = new Set();
+            Set s2 = new Set();
+
+            //roll new data to each set
+            ReRollData(s1, s2);
+
+            //print the two sets
+            Console.WriteLine("Generated two sets: ");
+            Console.WriteLine($"S1 ==> {s1}");
+            Console.WriteLine($"S2 ==> {s2}\n");
+
+            //Intersect the two sets
+            s1.Intersect(s2);
+            Console.WriteLine($"Intersecting S2 to S1 ==> {s1}\n");
+
+            //reset, reRoll and print data 
+            s1 = new Set();
+            s2 = new Set();
+            ReRollData(s1, s2);
+            Console.WriteLine("Rerolled Data!");
+            Console.WriteLine($"S1 ==> {s1}");
+            Console.WriteLine($"S2 ==> {s2}\n");
+
+            //Preform Union on the two sets
+            s1.Union(s2);
+            Console.WriteLine($"Union S2 and S1 to S1 ==> {s1}\n");
+
+            //reset, reRoll and print data 
+            s1 = new Set();
+            s2 = new Set();
+            ReRollData(s1, s2);
+            Console.WriteLine("Rerolled Data!");
+            Console.WriteLine($"S1 ==> {s1}");
+            Console.WriteLine($"S2 ==> {s2}\n");
+
+            //Create a custom user genetated Set
+            Console.WriteLine("Generating a user defined 3 numbers set: ");
+            Set s3 = new Set(
+                RequestNumber("Plase enter the first number: "), 
+                RequestNumber("Plase enter the second number: "), 
+                RequestNumber("Plase enter the third number: ")
+                );
+
+            //Check if s3 is a subset of one of the sets
+            Console.WriteLine($"\nChecking if the user's set is a subset of S1 or S2...");
+            if (s1.Subset(s3))
+            {
+                Console.WriteLine($"The user's set ==> {s3}\nis a subset of S1 ==> {s1}");
+            }
+            else
+            {
+                Console.WriteLine($"The user's set ==> {s3}\nis not a subset of S1 ==> {s1}");
+            }
+            if (s2.Subset(s3))
+            {
+                Console.WriteLine($"The user's set ==> {s3}\nis a subset of S2 ==> {s2}");
+            }
+            else
+            {
+                Console.WriteLine($"The user's set ==> {s3}\nis not a subset of S2 ==> {s2}");
+            }
+
+            //spacing
+            Console.WriteLine();
+
+            //check if a user input is a member of either set (s1,s2)
+            int num = RequestNumber("Please enter a number to see if it's a member of S1/S2: ");
+            Console.WriteLine($"Is {num} a member of S1: {s1.IsMember(num)}");
+            Console.WriteLine($"Is {num} a member of S2: {s2.IsMember(num)}\n");
+
+            //Insert a user input to s1 and s2
+            num = RequestNumber("Plase enter a number to insert to s1 and s2: ");
+            s1.Insert(num);
+            s2.Insert(num);
+            Console.WriteLine("Updated Sets Data: ");
+            Console.WriteLine($"S1 ==> {s1}");
+            Console.WriteLine($"S2 ==> {s2}\n");
+
+            //Delete a user input from s1 and s2
+            num = RequestNumber("Plase enter a number to delete from s1 and s2: ");
+            s1.Delete(num);
+            s2.Delete(num);
+            Console.WriteLine("Updated Sets Data: ");
+            Console.WriteLine($"S1 ==> {s1}");
+            Console.WriteLine($"S2 ==> {s2}\n");
+        }
+    }
+}
---
+++ Assignments/Sets/Set.cs
@@ -0,0 +1,132 @@
+﻿using System.Text;
+
+namespace Sets
+{
+    class Set
+    {
+        //represent the maximum range (the last index of the boolean array)
+        const int TopRange = 1000;
+
+        //a private boolean array as requested
+        private bool[] _array;
+
+        //an empty constructor will initialize the array
+        public Set()
+        {
+            _array = new bool[TopRange + 1];
+        }
+
+        //constructor with parmas, first it will go to the empty constructor(where it will initialize the array)
+        //than it will execute the logic in the block
+        public Set(params int[] numbers) : this()
+        {
+            foreach (int num in numbers)
+            {
+                if (IsInRange(num))
+                {
+                    _array[num] = true;
+                }
+            }
+        }
+
+        //Union combines two sets without duplicates [since we are using a boolean array, each number(index) can appear only once]
+        public void Union(Set set)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = set._array[i] || _array[i];
+            }
+        }
+
+        //Intersect will change the set to only having the shared data with the parameter Set
+        public void Intersect(Set set)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = set._array[i] && _array[i];
+            }
+        }
+
+        //Subset will return true if a set contains all of the numbers from the parameter set
+        //an empty set, is a subset off all other sets
+        public bool Subset(Set set)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (set._array[i] && !_array[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //IsMember will return true if the parameter is in the set
+        public bool IsMember(int num)
+        {
+            return IsInRange(num) && _array[num];
+        }
+
+        //Insert will add a number to the set
+        public void Insert(int num)
+        {
+            if (IsInRange(num))
+            {
+                _array[num] = true;
+            }
+        }
+
+        //Delete will remove the parameter number from the set
+        public void Delete(int num)
+        {
+            if (IsInRange(num))
+            {
+                _array[num] = false;
+            }
+        }
+
+        //Will return a string of the set
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i])
+                {
+                    sb.Append($"{i} ");
+                }
+            }
+            return sb.ToString();
+        }
+
+        //compares two sets to see if they share the exact same Data
+        public override bool Equals(object obj)
+        {
+            //reflect to see if obj can be downcasted to Set
+            if (obj.GetType() == typeof(Set))
+            {
+                Set temp = (Set)obj;
+                for (int i = 0; i < _array.Length; i++)
+                {
+                    if (temp._array[i] != _array[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            //if the two objects are not of the same type, they aren't comparable therefore will return false
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        //a simple boolean function to avoid coding the same line multiple times
+        private bool IsInRange(int num)
+        {
+            return num >= 0 && num <= TopRange;
+        }
+    }
+}
